a13ddce66ef8f3146d9d241ecabb9e3c7d7447d4
AyrtonCossuol <ayrton.cossuol@live.com>
2020-06-25 16:41:04 -0300

    registration working

--- tasks/src/screens/Auth/Auth.js
+++ tasks/src/screens/Auth/Auth.js
@@ -7,26 +7,49 @@ import {
     TouchableOpacity, 
     Alert,
 } from 'react-native';
+import axios from 'axios';
 
 import styles from './styles';
 
 import backgroundImage from '../../../assets/imgs/login.jpg';
 import AuthInput from '../../components/AuthInput/AuthInput';
 
+import { server, showError, showSuccess } from '../../common';
+
+const initialState = { 
+    name: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    stageNew: false,
+};
+
 export default class Auth extends Component {
     state = {
-        name: '',
-        email: '',
-        password: '',
-        confirmPasseord: '',
-        stageNew: false,
+        ...initialState
     };
 
     signinOrSignup = () => {
         if(this.state.stageNew) {
-            Alert.alert('Sucesso', 'Criar conta');
+            this.signup()
         } else {
             Alert.alert('Sucesso', 'Logar');
+        }
+    };
+
+    signup = async () => {
+        try{
+            await axios.post(`${server}/signup`, {
+                name: this.state.name,
+                email: this.state.email,
+                password: this.state.password,
+                confirmPassword: this.state.confirmPassword,
+            });
+
+            showSuccess('Usuario cadastrado!');
+            this.setState({ ...initialState });
+        } catch(e){
+            showError(e)
         }
     };
 
@@ -69,8 +92,8 @@ export default class Auth extends Component {
                             icon='asterisk'
                             style={styles.input} 
                             placeholder='Confirmação da Senha' 
-                            value={this.state.confirmPasseord} 
-                            onChangeText={confirmPasseord => this.setState({ confirmPasseord })}
+                            value={this.state.confirmPassword} 
+                            onChangeText={confirmPassword => this.setState({ confirmPassword })}
                             secureTextEntry={true}
                         />
                     }
